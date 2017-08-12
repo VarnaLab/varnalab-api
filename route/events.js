564fc6059d2b2a2588cef31fa43b4152e3397250
simov <simeonvelichkov@gmail.com>
2017-08-12 21:43:04 +0300

    Add upcoming events route and find by id

--- route/events.js
+++ route/events.js
@@ -6,9 +6,32 @@ module.exports = (db) => {
   var api = express()
 
   api.get('/', (req, res) => {
-    var offset = parseInt(req.query.offset || 0)
-    var limit = parseInt(req.query.limit || 10)
-    res.json(db.state.events.slice(offset, offset + limit))
+    if (req.query.id) {
+      var id = req.query.id
+      res.json(db.state.events.find((event) => event.id === id))
+    }
+    else {
+      var offset = parseInt(req.query.offset || 0)
+      var limit = parseInt(req.query.limit || 10)
+      res.json(db.state.events.slice(offset, offset + limit))
+    }
+  })
+
+  api.get('/upcoming', (req, res) => {
+    var now = new Date().getTime()
+    var upcoming = []
+
+    for (var event of db.state.events) {
+      var start = new Date(event.start_time).getTime()
+      if (start >= now) {
+        upcoming.push(event)
+      }
+      else {
+        break
+      }
+    }
+
+    res.json(upcoming)
   })
 
   return api
